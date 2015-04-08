f0e8e7766f9d11991924aa1e0210bc1ebcf47ba3
Ormada <odabney@gmail.com>
2015-04-08 14:04:24 -0500

    Powershell
    Stuff

---
+++ DistribOutput.ps1
@@ -0,0 +1,7 @@
+﻿Add-PSSnapin microsoft.exchange.management.powershell.e2010
+$group = Get-DistributionGroupMember partners-all |where-object{$_.RecipientType -eq "MailUniversalSecurityGroup"}
+
+Foreach ($user in $group.Name) {
+    Get-DistributionGroupMember $user |ft Name, StateorProvince
+    }
+    
---
+++ Services_Test.ps1
@@ -0,0 +1,23 @@
+﻿$servers = Get-ADComputer -filter {OperatingSystem -Like "Windows Server*"}  -Property * | ? {$_.DistinguishedName -eq "CN=MP*"} 
+foreach ($server in $servers.DistinguishedName)
+{
+    
+
+    $status = Get-Service -computername $server | ? {$_.DisplayName -eq "Symantec Management Agent"}  
+    
+    if ($status.Status -eq "Stopped") 
+    {
+        get-service -name $status.Name -computername $server | set-service -Status Running
+        #write-host "Service was Started on $server"
+        out-file -filepath d:\scripts\Services_Output.txt -append -inputobject "$($status.Name) was Started on $server"
+
+        # Get-Service | ? {$_.DisplayName -eq "Symantec Management Agent"}
+
+    }  
+    else 
+    {
+        #write-host ""$status.Name" is already Running on $server"
+        #out-file -filepath d:\scripts\Services_Output.txt -append -InputObject  "$($status.Name) is already Running on $server"
+    }
+
+}
